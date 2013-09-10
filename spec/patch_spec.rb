44dafd7009892a04bbb1f0fc1017c35682b52e4d
Rémi Prévost <remi@exomel.com>
2013-09-10 16:20:59 -0400

    Add tests

---
+++ spec/patch_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe PatchPatch do
+  let(:routes) { ActionDispatch::Routing::RouteSet.new }
+  let(:route_details) do
+    routes.routes.routes.inject([]) do |memo, route|
+      memo << {
+        controller: route.defaults[:controller],
+        action: route.defaults[:action],
+        method: route.constraints[:request_method].source.gsub(/[^A-Z]+/, '')
+      }
+    end
+  end
+
+  context 'with all default actions' do
+    before do
+      routes.draw { resources :users }
+    end
+
+    it { expect(route_details.first).to eql({ controller: 'users', action: 'partial_update', method: 'PATCH' }) }
+  end
+
+  context 'with `partial_update`' do
+    before do
+      routes.draw { resources :users, except: [:partial_update] }
+    end
+
+    it { expect(route_details).to_not include({ controller: 'users', action: 'partial_update', method: 'PATCH' }) }
+  end
+end
